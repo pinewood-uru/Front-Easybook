99ba38ebbc7b75bc138e99e1e93555a80b30cc24
fran <f.avellanal96@gmail.com>
2024-06-29 18:07:54 -0300

    sabado2

---
+++ js/login.js
@@ -0,0 +1,25 @@
+import { Request } from "../Request.js";
+import { imprimir, obtenerValorInput, validarSesion } from "../util/helpers.js";
+
+// validamos la sesion del usuario
+validarSesion();
+
+const btnLogin = document.getElementById("form-login-submit");
+
+btnLogin.addEventListener("click", () => {
+    const email = obtenerValorInput("email");
+    const password = obtenerValorInput("password");
+    Request.login(email, password)
+    .then((data) => {
+       
+        sessionStorage.setItem("session", data.session);
+        sessionStorage.setItem("user", JSON.stringify(data.user));
+        
+        document.location.replace("index.html");
+    })
+    .catch((error) => {
+        console.log(error);
+        imprimir("form-login-error", error)
+    })
+
+})
